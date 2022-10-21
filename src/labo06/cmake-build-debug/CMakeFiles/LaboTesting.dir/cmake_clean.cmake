88b16acbcfc1b9610ea19048a1ffc2a635d3b17e
jotavecorta <julianvilla.90@gmail.com>
2022-10-21 14:57:54 -0300

    Agregué Labo6. Primera version

---
+++ src/labo06/cmake-build-debug/CMakeFiles/LaboTesting.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LaboTesting.dir/ej1/Test/esPrimoTest.cpp.o"
+  "CMakeFiles/LaboTesting.dir/ej1/esPrimo.cpp.o"
+  "CMakeFiles/LaboTesting.dir/main.cpp.o"
+  "LaboTesting"
+  "LaboTesting.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LaboTesting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
