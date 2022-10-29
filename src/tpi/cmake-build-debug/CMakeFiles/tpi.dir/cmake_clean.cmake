a0eeaa9cb15a1f284090d120120773ff52afcc23
jotavecorta <julianvilla.90@gmail.com>
2022-10-29 15:28:25 -0300

    Agregue TPI. Agregué 9 primeros tests a minasAdyacentesTEST.cpp: 5 casos borde, 4 casos en interior del tablero.

---
+++ src/tpi/cmake-build-debug/CMakeFiles/tpi.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "../tpi"
+  "../tpi.pdb"
+  "CMakeFiles/tpi.dir/auxiliares.o"
+  "CMakeFiles/tpi.dir/ejercicios.o"
+  "CMakeFiles/tpi.dir/main.o"
+  "CMakeFiles/tpi.dir/tests/cambiarBanderitaTEST.o"
+  "CMakeFiles/tpi.dir/tests/ganoTEST.o"
+  "CMakeFiles/tpi.dir/tests/jugarPlusTEST.o"
+  "CMakeFiles/tpi.dir/tests/minasAdyacentesTEST.o"
+  "CMakeFiles/tpi.dir/tests/perdioTEST.o"
+  "CMakeFiles/tpi.dir/tests/sugerir121TEST.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tpi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
