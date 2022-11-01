efe60bd882b1f50821483c3ef829b79e72096bd8
Gon <gspina1995@gmail.com>
2022-11-01 18:20:26 -0300

    Commented things out

--- src/tpi/auxiliares.cpp
+++ src/tpi/auxiliares.cpp
@@ -97,14 +97,17 @@ bool perteneceEnJugadas(jugada k, jugadas j) {
 bool es121Horizontal(pos p, jugadas j){
     // indiceJugadas % m != 0 && indiceJugadas % m != (m - 1)
     // return perteneceEnJugadas(((p.first,p.second - 1),1),j) and perteneceEnJugadas(((p[0],p[1]),2),j) and perteneceEnJugadas(((p[0],p[1] + 1),1),j);
+    return true;
 }
 
 bool es121Vertical(pos p, jugadas j){
-  return perteneceEnJugadas(((p[0] - 1,p[1]),1),j) and perteneceEnJugadas(((p[0],p[1]),2),j) and perteneceEnJugadas(((p[0] + 1,p[1]),1),j);
+  // return perteneceEnJugadas(((p[0] - 1,p[1]),1),j) and perteneceEnJugadas(((p[0],p[1]),2),j) and perteneceEnJugadas(((p[0] + 1,p[1]),1),j);
+    return true;
 }
 
 bool esAdyacenteA121(pos p, jugadas j){
-  return es121Horizontal((p[0] - 1,p[1]),j) or es121Horizontal((p[0] + 1,p[1]),j) or es121Vertical((p[0],p[1] - 1),j) or es121Vertical((p[0],p[1] + 1),j);
+  // return es121Horizontal((p[0] - 1,p[1]),j) or es121Horizontal((p[0] + 1,p[1]),j) or es121Vertical((p[0],p[1] - 1),j) or es121Vertical((p[0],p[1] + 1),j);
+    return true;
 }
 
 bool hayPosicionSugerible(jugadas j, banderitas b, tablero t){
