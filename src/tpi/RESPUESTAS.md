6d25cad71ccb1183e2c029600325c557448c4701
Gon <gspina1995@gmail.com>
2022-12-04 21:14:36 -0300

    Modifiqué respuestas.md

--- src/tpi/RESPUESTAS.md
+++ src/tpi/RESPUESTAS.md
@@ -1,23 +1,21 @@
 ﻿### 2.4 Sobre la estructura de ``jugadas``:
 
-En nuestra implementación actual, la complejidad de ``jugarPlus`` es $O(|t|\cdot|t|)$,
+En nuestra implementación actual, la complejidad de ``jugarPlus`` es $O(|t|\cdot|t|\cdot|j|)$,
 mientras que la de ``sugerirAutomatico121`` es $O(|t|\cdot|t|\cdot|j|)$, donde $|t|$ y $|j|$
 representan el largo del vector de jugadas ``j`` y uno de los lados del tablero ``t``,
 respectivamente. En el caso de ``jugarPlus`` esto se debe a que posee una llamada recursiva, 
-que le permite en el peor de los casos recorrer todo el tablero. En cambio ``sugerirAutomatico121`` 
-recorre todo el tablero con dos loops, y para cada posición hace una búsqueda lineal
+que le permite en el peor de los casos recorrer todo el tablero y además una búsqueda lineal 
+en jugadas que es O(|j|) por cada posición de la misma. En cambio ``sugerirAutomatico121`` 
+recorre todo el tablero con dos loops, y para cada posición también hace una búsqueda lineal
 en el vector de jugadas ``j`` (recordemos que j no posee ningún orden particular). 
 
 Pasar a una implementación donde ``j`` es una matriz cuyos puntos corresponden a una
 posición del tablero (en una relación 1 a 1), y contienen el número de minas adyacentes
 a dicha posición, simplificaría las búsquedas sobre ``j``. Para saber si una posición ``(p, q)`` 
 se jugó, simplemente se accede a ``j[p][q]`` y se comprueba si es distinto de -1. 
-Esta operación se realiza en tiempo constante, i.e. es de complejidad O(1), por lo que 
-``sugerirAutomatico121`` pasaría de una complejidad $O(|t|\cdot|t|\cdot|j|)$ a $O(|t|\cdot|t|\cdot 1) = O(|t|\cdot|t|)$.
-Recordemos que en el peor de los casos $|j|$ es $|t|$ menos cantida total minas, que puede aproximarse mucho
-a |t|, por lo que pasaríamos de algo muy parecido a $|t|^3$, a $|t|^2$. 
-En el caso de ``jugarPlus``, aunque afectaría el tiempo de ejecución, no cambiaría su complejidad 
-puesto que ésta no depende de $|j|$.
-
+Esta operación se realiza en tiempo constante, i.e. es de complejidad O(1), por lo que ambos
+``jugarPlus`` y ``sugerirAutomatico121`` pasarían de una complejidad $O(|t|\cdot|t|\cdot|j|)$ a $O(|t|\cdot|t|\cdot 1) = O(|t|\cdot|t|)$.
+Recordemos que en el peor de los casos $|j|$ es $|t|²$ menos cantidad total minas, que puede aproximarse mucho
+a |t|, por lo que pasaríamos de algo muy parecido a $|t|^4$, a $|t|^2$.
 
 
--- src/tpi/auxiliares.cpp
+++ src/tpi/auxiliares.cpp
@@ -18,51 +18,6 @@ bool posicionValida(const tablero &t, int i, int j) {
     return (i>=0 && i < t.size()) && (j>=0 && j < t[0].size());
 }
 
-///*Mapea la posición (i, j) de un tablero de nxn en el indice correspondiente
-// *del vector de jugadas.*/
-//int mapIndex(int n, int i, int j) {
-//    return i*n + j;
-//}
-
-///*Modifica los vectores de jugadas y banderitas -j y b- asignando posiciones dentro de
-// *los mismos mediante una correspondencia 1 a 1 con las posiciones del tablero t. De esta
-// *manera los vectores j y b quedan inicializados para comenzar el juego, y pueden ser
-// *modificados a lo largo del mismo de manera estática y no dinámica. Esto reduce la complejidad
-// *de algoritmos subsiguientes.*/
-//void initJB(tablero& t, jugadas& j, banderitas& b) {
-//    // Si j ya está inicializado no hace nada.
-//    if (j.size() != 0) {
-//        return;
-//    }
-//
-//    /*
-//     Jugadas tiene correspondencia 1 a 1 con los casilleros de la matriz
-//
-//     0  1  2  3
-//     4  5  6  7
-//     8  9  10 11
-//     12 13 14 15
-//
-//     0           1           2           3
-//     0   1  2  3  4  5  6  7  8  9  10 11 12 13 14 15
-//     -1 -1  1 -1 -1 -1  2 -1 -1 -1  1  -1 -1 -1 -1 -1
-//
-//     Es decir la posición t[i][k] corresponde a j[i*m + k]
-//     Esto nos permite acceder a la información de la posición en 0[2]
-//    */
-//
-//    // Inicializamos j y b con valores que indican que las posiciones
-//    //no fueron jugadas ni se marcó una banderita
-//    int nm = t.size() * t[0].size();
-//    pos null_pos(-1, -1);
-//    jugada ju(null_pos, -1);
-//
-//    for (int i = 0; i < nm; ++i) {
-//        j.push_back(ju);
-//        b.push_back(null_pos);
-//    }
-//}
-
 /*Modifica el vector jugadas, agregando la posicion jugada -p- y sus minas
  *adyacentes al final del vector de jugadas j. Está compuesta de operaciones
  * O(1) -tanto minasAdyacentes como agregar un elemento al final de un vector-.*/
@@ -122,22 +77,3 @@ int estaEnBanderitas(const tablero &t, const banderitas &b, pos p) {
 
     return res;
 }
-
-///*Retorna la cantidad de minas adyacentes a la posición p del
-// *tablero t, teniendo en cuenta si ese calculo ya se realizó anteriormente.
-// *En caso de haberse realizado, toma dicho valor del vector j.*/
-//int minasAdyacentesWithCache(tablero& t, jugadas& j, pos p) {
-//    int index = mapIndex(t.size(), p.first, p.second);
-//    int c = j[index].second;
-//    if (c > -1) {
-//        return c;
-//    }
-//    return minasAdyacentes(t, p);
-//}
-
-///*Chequea si hay una banderita colocada en la posición p del tablero t,
-// *comprobando en el vector de banderitas b.*/
-//bool hayBanderita(tablero& t, banderitas& b, pos p) {
-//    int index = mapIndex(t.size(), p.first, p.second);
-//    return b[index].first != -1;
-//}
